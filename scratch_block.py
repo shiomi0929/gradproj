b6cc03dd3125bce5cd5e14786fe108b82a842e36
shiomi0929 <g13924ms@gm.tsuda.ac.jp>
2016-11-01 16:16:04 +0900

    updated

--- scratch_block.py
+++ scratch_block.py
@@ -1,29 +1,26 @@
 #動き
 
-{'forward':0, 'turnRight':0, 'turnLeft':0, 'heading':0, 'pointTowards':0, 'gotoX:y:':0, 
-
+{
+    'forward':0,
+    'turnRight':0,
+    'turnLeft':0,
+    'heading':0,
+    'pointTowards':0,
+    'gotoX:y':0,
+    'gotoSpriteOrMouse':0,
+    'glideSecs:toX:y:elapsed:from':0,
+    'changeXposBy':0,
+    'xpos':0,
+    'changeYposBy':0,
+    'ypos':0,
+    'bounceOffEdge':0,
+    'setRotationStyle':0,
+    'xpos':0,
+    'ypos':0,
+    'heading':0
+}
 #見た目
 
-{'say:duration:elapsed:from':0, 
- 'say':0, 
- 'think:duration:elapsed:from':0, 
- 'think':0, 
- 'show':0, 
- 'hide':0, 
- 'lookLike':0, 
- 'nextCostume':0, 
- 'startScene':0, 
- 'changeGraphicEffect:by':0,
- 'setGraphicEffect:to':0,
- 'filterReset':0,
- 'changeSizeBy':0,
- '':0,
- '':0,
- '':0,
- '':0,
- '':0,
- 
- 
 #音
 
 #ペン
@@ -31,7 +28,72 @@
 #データ
 
 #イベント
-
+{
+    'whenGreenFlag':0,
+    'whenKeyPressed':0,
+    'whenClicked':0,
+    'whenSceneStarts':0,
+    'whenSensorGreaterThan':0,
+    'whenIReceive':0,
+    'broadcast':0,
+    'doBroadcastAndWait':0
+}
 #制御
+{
+    'wait:elapsed:from':0,
+    'doRepeat':0,
+    'doForever':0,
+    'doIf':0,
+    'doIfElse':0,
+    'doWaitUntil':0,
+    'doUntil':0,
+    'stopScripts':0,
+    'whenCloned':0,
+    'createCloneOf':0,
+    'deleteClone':0
+}
 
-#
+#調べる
+{
+    'touching':0,
+    'touchingColor':0,
+    'color:sees:':0,
+    'distanceTo:':0,
+    'doAsk':0,
+    'answer':0,
+    'keyPressed':0,
+    'mousePressed':0,
+    'mouseX':0,
+    'mouseY':0,
+    'soundLevel':0,
+    'senseVideoMotion':0,
+    'setVideoState':0,
+    'setVideoTransparency':0,
+    'timer':0,
+    'timerReset':0,
+    'getAttribute:of':0,
+    'timeAndDate':0,
+    'timestamp':0,
+    'getUserName':0
+}
+
+#演算
+{
+    '+':0,
+    '-':0,
+    '*':0,
+    '/':0,
+    'randomFrom:to:':0,
+    '<':0,
+    '=':0,
+    '>':0,
+    '&':0,
+    '|':0,
+    'not':0,
+    'concatenate:with':0,
+    'letter:of':0,
+    'stringLength':0,
+    '%':0,
+    'rounded':0,
+    'computeFunction:of':0
+}

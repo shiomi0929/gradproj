f3880d0bcb86f2958d696166ce4b6d9984834d9f
shiomi <g13924ms@gm.tsuda.ac.jp>
2016-11-02 17:32:40 +0900

    no message

--- scratch_block.py
+++ scratch_block.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 #動き
-
 {
     'forward:':0,
     'turnRight':0,
@@ -18,10 +17,8 @@
     'setRotationStyle':0,
     'xpos':0,
     'ypos':0,
-    'heading':0
-}
+    'heading':0,
 #見た目
-{
     'say:duration:elapsed:from:':0,
     'say:':0,
     'think:duration:elapsed:from:':0,
@@ -41,9 +38,7 @@
     #'costumeIndex':0,
     #'sceneName':0,
     #'scale':0
-}
 #音
-{
     'playSound':0,
     'doPlaySoundAndWaid':0,
     'stopAllSounds':0,
@@ -57,10 +52,7 @@
     #'changeTempo:':0,
     'setTempoTo:':0,
     #'tempo':0
-}
-
 #ペン
-{
     'clearPenTrails':0,
     'stampCostume':0,
     'putPenDown':0,
@@ -71,11 +63,8 @@
     'changePenShadeBy:':0,
     'setPenShadeTo:':0,
     'changePenSizeBy:':0,
-    'penSize:':0
-}
-
+    'penSize:':0,
 #データ
-{
     'readVariable':0,
     'setVar:to:':0,
     'changeVar:by:':0,
@@ -90,11 +79,8 @@
     'lineCountOfList:':0,
     'list:contains:':0,
     'showList:':0,
-    'hideList:':0
-}
-
+    'hideList:':0,
 #イベント
-{
     'whenGreenFlag':0,
     'whenKeyPressed':0,
     'whenClicked':0,
@@ -102,10 +88,8 @@
     'whenSensorGreaterThan':0,
     'whenIReceive':0,
     'broadcast':0,
-    'doBroadcastAndWait':0
-}
+    'doBroadcastAndWait':0,
 #制御
-{
     'wait:elapsed:from':0,
     'doRepeat':0,
     'doForever':0,
@@ -116,11 +100,8 @@
     'stopScripts':0,
     'whenCloned':0,
     'createCloneOf':0,
-    'deleteClone':0
-}
-
+    'deleteClone':0,
 #調べる
-{
     'touching':0,
     'touchingColor':0,
     'color:sees:':0,
@@ -140,11 +121,8 @@
     'getAttribute:of':0,
     'timeAndDate':0,
     'timestamp':0,
-    'getUserName':0
-}
-
+    'getUserName':0,
 #演算
-{
     '+':0,
     '-':0,
     '*':0,

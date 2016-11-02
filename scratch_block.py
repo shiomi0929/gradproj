7485a23dedc559cf48d74876c941fbf88fe10342
shiomi <g13924ms@gm.tsuda.ac.jp>
2016-11-02 17:17:23 +0900

    Block追加しました。

--- scratch_block.py
+++ scratch_block.py
@@ -1,18 +1,19 @@
+# -*- coding: utf-8 -*-
 #動き
 
 {
-    'forward':0,
+    'forward:':0,
     'turnRight':0,
     'turnLeft':0,
-    'heading':0,
-    'pointTowards':0,
-    'gotoX:y':0,
+    'heading:':0,
+    'pointTowards:':0,
+    'gotoX:y:':0,
     'gotoSpriteOrMouse':0,
-    'glideSecs:toX:y:elapsed:from':0,
-    'changeXposBy':0,
-    'xpos':0,
-    'changeYposBy':0,
-    'ypos':0,
+    'glideSecs:toX:y:elapsed:from:':0,
+    'changeXposBy:':0,
+    'xpos:':0,
+    'changeYposBy:':0,
+    'ypos:':0,
     'bounceOffEdge':0,
     'setRotationStyle':0,
     'xpos':0,
@@ -20,12 +21,77 @@
     'heading':0
 }
 #見た目
-
+{
+    'say:duration:elapsed:from:':0,
+    'say:':0,
+    'think:duration:elapsed:from:':0,
+    'think:':0,
+    'show':0,
+    'hide':0,
+    'lookLike:':0,
+    'nextCostume':0,
+    'startScene':0,
+    'changeGraphicEffect:by:':0,
+    'setGraphicEffect:to:':0,
+    'filterReset':0,
+    'changeSizeBy:':0,
+    'setSizeTo:':0,
+    'comeToFront':0,
+    'goBackByLayers:':0,
+    #'costumeIndex':0,
+    #'sceneName':0,
+    #'scale':0
+}
 #音
+{
+    'playSound':0,
+    'doPlaySoundAndWaid':0,
+    'stopAllSounds':0,
+    'playDrum':0,
+    'rest:elapsed:from:':0,
+    'noteOn:duration;elapsed;from;':0,
+    'instrument':0,
+    'changeVolumeBy:':0,
+    'setVolumeTo:':0,
+    #'volume':0,
+    #'changeTempo:':0,
+    'setTempoTo:':0,
+    #'tempo':0
+}
 
 #ペン
+{
+    'clearPenTrails':0,
+    'stampCostume':0,
+    'putPenDown':0,
+    'putPenUp':0,
+    'penColor:':0,
+    'changePenHueBy:':0,
+    'setPenHueTo:':0,
+    'changePenShadeBy:':0,
+    'setPenShadeTo:':0,
+    'changePenSizeBy:':0,
+    'penSize:':0
+}
 
 #データ
+{
+    'readVariable':0,
+    'setVar:to:':0,
+    'changeVar:by:':0,
+    'showVariable:':0,
+    'hideVariable:':0,
+    'contentsOfList:':0,
+    'append:toList':0,
+    'deleteLine:ofList:':0,
+    'insert:at:ofList:':0,
+    'setLine:ofList:to:':0,
+    'getLine:ofList':0,
+    'lineCountOfList:':0,
+    'list:contains:':0,
+    'showList:':0,
+    'hideList:':0
+}
 
 #イベント
 {

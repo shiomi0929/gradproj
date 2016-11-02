40cbad1fe487cf539f341c7e719ca385bf4ce532
shiomi <g13924ms@gm.tsuda.ac.jp>
2016-11-02 18:40:45 +0900

    できなくなっちゃった。。。。。

--- jsonSamp.py
+++ jsonSamp.py
@@ -1,35 +1,31 @@
 import json
-x = "crabsam.json"
+x = "neko.json"
 f = open(x,'r')
 data = json.load(f)
 y = data["children"]
 s = y[0][u'scripts']
 
 def printFirst(L):
-    print "start"
     print L
     if isinstance(L, list):
-	first = L[0]
+        first = L[0]
         if isinstance(first, unicode):
-            print "first"
             print first 
             for e in L:
                 printFirst(e)
-    print "end"
-        
+                
 def getFirst(L, dict):
     if isinstance(L, list):
         first = L[0]
-        if isinstance(first,unicode):
+        if isinstance(first, unicode):
             if first in dict:
                 dict[first] += 1
             else:
                 dict[first] = 1
-        for e in L:
-            getFirst(e,dict) 
-                
-print s
-printFirst(s)
+            for e in L:
+                getFirst(e, dict)
+                    
+#printFirst(s)
 dictOne = {}
 getFirst(s, dictOne)
 print dictOne

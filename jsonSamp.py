2796201cd735dc3de7315b310814ca366412b9af
chisae <g13908ic+git@gm.tsuda.ac.jp>
2016-11-03 12:23:53 +0900

    Add files via upload
    なおったよ！

--- jsonSamp.py
+++ jsonSamp.py
@@ -4,15 +4,16 @@ f = open(x,'r')
 data = json.load(f)
 y = data["children"]
 s = y[0][u'scripts']
+#print s
 
 def printFirst(L):
-    print L
+    #print L
     if isinstance(L, list):
         first = L[0]
         if isinstance(first, unicode):
             print first 
-            for e in L:
-                printFirst(e)
+        for e in L:
+            printFirst(e)
                 
 def getFirst(L, dict):
     if isinstance(L, list):
@@ -22,8 +23,8 @@ def getFirst(L, dict):
                 dict[first] += 1
             else:
                 dict[first] = 1
-            for e in L:
-                getFirst(e, dict)
+        for e in L:
+            getFirst(e, dict)
                     
 #printFirst(s)
 dictOne = {}
